a1d50c0518fbecd87d9352ab33d2ba0c904c2135
Ansh Jaiswal <anshjaiswal9323@gmail.com>
2026-04-09 21:53:02 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 // ── Middleware ────────────────────────────────────────────
 app.use(cors({
   origin: '*',
-  credentials: true
+  credentials: false
 }));
 app.use(express.json());
 
